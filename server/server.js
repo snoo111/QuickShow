5e2265df7cc5dd8b0dc1776b299e53d3edd49a19
snoo111 <snehawaiba333@gmail.com>
2026-06-17 09:06:15 +0545

    initial setup:express server, mongodb connection

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+console.log("MongoDB URI:", process.env.MONGODB_URI)
+
+import express from 'express';
+import cors from 'cors';
+
+import connectDB from './config/db.js';
+
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
+const app = express();
+const port = 3000;
+
+await connectDB()
+
+//Middleware
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware())
+
+
+//API Routes
+app.get('/', (req, res) => 
+    res.send('Server is live!'))
+app.use('/api/inngest',serve({ client: inngest, functions} ))
+
+app.listen(port,()=> console.log(`Server listening at http://localhost:${port}`));
